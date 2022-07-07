631920aca00668e06a4274d922dfdb7a1a4e0b24
Matias Rico <matiasrico28@gmail.com>
2022-07-07 20:31:50 -0300

    se agrega libreria

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -79,3 +79,18 @@ boton.addEventListener('click', solicitar)
 function solicitar(){
     console.log('Gracias por solicitar nuestro servicio, nos contactaremos contigo a la brevedad')
 }
+
+const submitForm = (ID) => {
+    let form =document.getElementById(ID);
+    form.addEventListener('submit', (e) =>{
+        e.preventDefault(); 
+        console.log(e.target);
+        console.log('submitForm')
+        console.log(form.children)
+    })
+}
+submitForm('formulario')
+
+const dt = DateTime.now();
+
+console.log( dt.toLocaleString(DateTime.DATETIME_SHORT) )
