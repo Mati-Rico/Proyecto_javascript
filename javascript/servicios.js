3ac5c8de263819859ce2fe5cc4c262c45bcfc81d
Matias Rico <matiasrico28@gmail.com>
2022-08-01 21:41:58 -0300

    agregando javascript pagina servicios

--- javascript/servicios.js
+++ javascript/servicios.js
@@ -5,3 +5,33 @@ fetch("https://api.kanye.rest")
     .then((response) => response.json())
     .then((data) => (divWithQuote.innerHTML = data.quote));
 };
+
+const showAlert = () => {
+    Swal.fire({
+        title:'Equipo Electrico',
+        text:'El costo de adquirir este servicio es de $750',
+        icon:'submit',
+        confirmButtonText:'Cerrar'
+    }
+    )
+}
+
+let button = document.getElementById('cotElectrico');
+button.onclick = showAlert
+
+function multiplicar() {
+    let cantidad = document.getElementById('CantidadElectricos').value;
+    const precioElectrico = 800;
+
+    let producto = cantidad * precioElectrico;
+
+    document.getElementById('resultado').innerText = producto;
+}
+function multiplicar2() {
+    let cantidad2 = document.getElementById('CantidadDigital').value;
+    const precioDigital = 750;
+
+    let producto2 = cantidad2 * precioDigital;
+
+    document.getElementById('resultado2').innerText = producto2;
+}
